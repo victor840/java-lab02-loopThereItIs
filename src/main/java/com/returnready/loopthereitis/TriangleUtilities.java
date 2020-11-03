3eb2228c6bab3fdc97b509a9bf10737b89ec2487
victor840 <victor.ugoanusi@gmail.com>
2020-11-02 23:53:03 -0500

    add int method

--- src/main/java/com/returnready/loopthereitis/TriangleUtilities.java
+++ src/main/java/com/returnready/loopthereitis/TriangleUtilities.java
@@ -1,20 +1,55 @@
 package com.returnready.loopthereitis;
 
+import java.util.ArrayList;
+
 public class TriangleUtilities {
 
     public static String getTriangle(int numberOfRows) {
-        return null;
+         int numberOfStars = 1;
+         String stars = "";
+        for (int i = 1; i < numberOfRows; i++){
+            for(int j = 0; j < numberOfStars; j++){
+                stars += "*";
+            }
+            numberOfStars++;
+            stars += "\n";
+        }
+        return stars;
     }
 
     public static String getRow(int numberOfStars) {
-        return null;
+        String stars = "";
+        for (int i = 0; i < numberOfStars; i++){
+            stars += "*";
+        }
+        return stars;
     }
 
     public static String getSmallTriangle() {
-        return null;
+        int numberOfRows = 5;
+        int numberOfStars = 1;
+        String stars = "";
+        for (int i = 1; i < numberOfRows; i++){
+            for(int j = 0; j < numberOfStars; j++){
+                stars += "*";
+            }
+            numberOfStars++;
+            stars += "\n";
+        }
+        return stars;
     }
 
     public static String getLargeTriangle() {
-        return null;
+        int numberOfRows = 10;
+        int numberOfStars = 1;
+        String stars = "";
+        for (int i = 1; i < numberOfRows; i++){
+            for(int j = 0; j < numberOfStars; j++){
+                stars += "*";
+            }
+            numberOfStars++;
+            stars += "\n";
+        }
+        return stars;
     }
 }
